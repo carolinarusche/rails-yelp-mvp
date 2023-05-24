c9abbb609df4d33b9f35937780ff4df7e6ccc9a4
Carolina Rusche <carolinarusche@gmail.com>
2023-05-24 16:43:17 +0200

    Challenge completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  Restaurant.create(name: Faker::Movie.quote, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number_with_country_code, category: "italian")
+end
